e8da1ddfc45cd46c0e3a43d5015171425e72e582
sora <133684349+sorawaslocked@users.noreply.github.com>
2025-04-24 16:22:55 +0500

    Add movie filter and update data structs

--- internal/model/movie.go
+++ internal/model/movie.go
@@ -14,3 +14,37 @@ type Movie struct {
 	CreatedAt        time.Time
 	UpdatedAt        time.Time
 }
+
+type MovieFilter struct {
+	ID                    *string
+	Ratings               []string
+	PrimaryTitle          *string
+	OriginalTitle         *string
+	ReleaseYearRange      *ReleaseYearRange
+	RuntimeInMinutesRange *RuntimeInMinutesRange
+	Genres                []string
+	InTheatricalRun       *bool
+}
+
+// ReleaseYearRange is inclusive
+type ReleaseYearRange struct {
+	YearFrom uint16
+	YearTo   uint16
+}
+
+// RuntimeInMinutesRange is inclusive
+type RuntimeInMinutesRange struct {
+	RuntimeFrom uint16
+	RuntimeTo   uint16
+}
+
+type MovieUpdateData struct {
+	Rating           *string // Rating as in PG-13, R, etc.
+	PrimaryTitle     *string
+	OriginalTitle    *string
+	ReleaseYear      *uint16
+	RuntimeInMinutes *uint16
+	Genres           []string
+	InTheatricalRun  *bool
+	UpdatedAt        time.Time
+}
